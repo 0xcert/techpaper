ae0a5a179b28f597c4e3e36b5687f524365e993d
xpeper <xpepermint@gmail.com>
2018-03-16 17:27:01 +0100

    fixing padding

--- src/runnings.js
+++ src/runnings.js
@@ -1,22 +1,31 @@
 exports.header = {
-  height: '1cm',
+  height: '3cm',
+  contents: function(page, total) {
+    return '';
+  },
 };
 
 exports.footer = {
-  height: '1cm',
+  height: '3cm',
   contents: function(page, total) {
     return [
       '<footer>',
         '<style>',
+          // '@font-face {',
+          //   'font-family: "Latin Modern";',
+          //   `src: url("./fonts/lmroman10-regular.otf");`,
+          //   'font-weight: normal;',
+          //   'font-style: normal;',
+          // '}',
           'body {',
             'text-align: justify;',
-            'font-family: "Times New Roman";',
+            'font-family: "Latin Modern";',
             'font-size: 12px;',
             'line-height: 1.5;',
             'text-align: center;',
           '}',
         '</style>',
-        '<span>',
+        '<span><br/>0xcert protocol ',
           page > 3 ? page - 3 : '',
         '</span>',
       '</footer>'
